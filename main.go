398d133784e325ce562c3cf61d76600ab5891aa0
Angus Farquhar <mr.angus.farquhar@gmail.com>
2021-07-20 16:39:45 +1000

    Buckets map now added and serving simple calculation to local

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	salstr := r.URL.Path[1:]
+	fmt.Fprintf(w, "Your salary is %s.\n", salstr)
+
+	salInt, _ := strconv.Atoi(salstr)
+
+	buckets := makeBuckets(salInt)
+	fmt.Fprintf(w, "SPLURGE bucket is %v,\n", buckets["splurge"])
+	fmt.Fprintf(w, "SMILE bucket is %v,\n", buckets["smile"])
+	fmt.Fprintf(w, "FIRE EXTINGUISHER bucket is %v.\n", buckets["fire extinguisher"])
+
+}
+
+func makeBuckets(salary int) map[string]int {
+	buckets := make(map[string]int)
+	buckets["splurge"] += salary / 10
+	buckets["smile"] += salary / 10
+	buckets["fire extinguisher"] += salary / 5
+
+	return buckets
+}
+
+func main() {
+	//
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
